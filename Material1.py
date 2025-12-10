44c839cd83bc065b286a13a72642ff71deb8d308
Maksym <maksym.lazoryk@gmail.com>
2025-12-10 17:19:01 +0000

    woolf

--- Material1.py
+++ Material1.py
@@ -21,3 +21,13 @@
 #text = "Hello world"
 #new_text = text.replace("world", "Python")
 #print(new_text)
+
+
+
+#TASK 5
+#text = "one fish, two fish, red fish, blue fish" 
+#new_text = text.replace("fish", "bird", 2)
+#print(new_text)
+
+
+
