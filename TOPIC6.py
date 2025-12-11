b24f3c29c164a4e500d9876ad91d184cabba5682
Maksym <maksym.lazoryk@gmail.com>
2025-12-11 20:50:24 +0000

    WOOLF

--- TOPIC6.py
+++ TOPIC6.py
@@ -13,3 +13,9 @@
 # Import the math library for mathematical operations
 import math
 
+# This creates a function called "log_info"
+# A funciton is a block of reusable code that does a specific task
+# "message: str" means this funciton expects a text message as input
+# "-> None" means this function doesn't return any value, it just prints smth 
+def log_info(message: str) -> None:
+
